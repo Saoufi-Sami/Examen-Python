dd8a799e0803b46bae96e1dfb1767ae3877d596c
saoufi sami <saoufi-sami@hotmail.fr>
2026-02-10 08:29:54 +0100

    exo ok

--- exercice_cuisson_oeufs/main.py
+++ exercice_cuisson_oeufs/main.py
@@ -1,9 +1,8 @@
 import time
 
-TEMPS_COQUE = 1
+TEMPS_COQUE = 3
 TEMPS_MOLLETS = 6
 TEMPS_DURS = 3
-
 
 def afficher_temps_restant(temps_choisie):
     temps_restant_sec = temps_choisie * 60
@@ -14,16 +13,13 @@ def afficher_temps_restant(temps_choisie):
         print(".", end="", flush=True)
         
     print()
-
-
-
     while temps_restant_sec >= 0:
           minutes = temps_restant_sec // 60
           secondes = temps_restant_sec - (minutes * 60)
           print(f"Durée restante : {minutes:02d}:{secondes:02d}",end="", flush=True)
           if temps_restant_sec == 0:
              break
- # on utilise ce if break pour ne pas avoir les points a la derniere iteration 00:00
+ # on utilise ce if break pour ne pas avoir les points apres la derniere iteration 00:00
           for i in range(10):
              time.sleep(1)
              print(".", end="", flush=True)
@@ -33,23 +29,10 @@ def afficher_temps_restant(temps_choisie):
     print("Cuisson terminée !")
 
 
-            
-
-
-
-                                                                                        
-
-
-
-
-
-
-
 titre = "Ma cuisson préféré"
-
 largeur = len(titre) + 11        # Longueur du titre + 11
 
-print("*" * largeur)   # Affiche autant d'etoile que le titre et rajoute en 11
+print("*" * largeur)            # Affiche autant d'etoile que le titre et rajoute en 11
 print(titre.center(largeur))    # Affiche mon titre centré avec le même nombre de caractères que la variable largeur.
 print("Oeufs a la coque : 3 minutes".center(largeur))
 print("Oeufs mollets    : 6 minutes".center(largeur))
