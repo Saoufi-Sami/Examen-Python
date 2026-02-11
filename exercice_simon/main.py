e1d1b3c4a916b298a2a46d96cc3db8ff33f6da4e
saoufi sami <saoufi-sami@hotmail.fr>
2026-02-11 10:22:16 +0100

    Exo Simon ok

--- exercice_simon/main.py
+++ exercice_simon/main.py
@@ -11,14 +11,51 @@ def clear_screen():
     else:
         os.system('cls')
 
-print("Retenez la séquence" )
+
 
 for i in range (0,4):
     sequence_initial = (random.randint(0,9))
-    print(sequence_initial,end="" )
     sequence_memoire= str(sequence_memoire) + str(sequence_initial)
-print()
-time.sleep(3)
-clear_screen()
 
-votre_reponse = input("Votre reponse : ")
+while True :
+    print("Retenez la séquence")
+    time.sleep(1)
+    clear_screen()
+
+    print(sequence_memoire, end="")
+    time.sleep(3)
+    clear_screen()
+
+    votre_reponse = input("Votre reponse : ")
+    print()
+
+    if votre_reponse == sequence_memoire:
+        score += 1
+        sequence_memoire = sequence_memoire + str(random.randint(0, 9))
+        print("Bonne reponse votre score est : " + str(score))
+        time.sleep(1)
+        clear_screen()
+    else:
+        print("Mauvaise réponse, la séquence était : " + str(sequence_memoire) )
+        print("Votre score final est : " +str(score))
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
